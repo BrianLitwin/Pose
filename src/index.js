cdaf27d9d2da7f462b27372d35a9d3ff5f8abc52
Brian Litwin <blitwin@horizoninvestments.com>
2023-10-02 17:31:25 -0400

    Refactor drawing on-screen
    console should log new pose when it's selected in menu
    a new function should not be spun up when this happens

--- src/index.js
+++ src/index.js
@@ -1,42 +1,55 @@
 import { createRoot } from 'react-dom/client';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import React from 'react';
 import {Video, startVideo} from './video';
 import { getDetector } from './detector';
-import { drawKeypoints } from './draw';
+import { drawKeypoints, drawLine } from './draw';
 import { Menu } from './menu';
+// import { drawLine } from './draw';
 
 const CONFIDENCE = 0.3
 
-async function start() {
+async function getConfig() {
     const {ctx, video} = await startVideo()
     const detector = await getDetector()
+    return {ctx, video, detector}
+}
 
-    async function drawVideo() {
+async function draw(pose, {ctx, video, detector}) {
+    const poses = await detector.estimatePoses(video, {flipHorizontal: false});
+    ctx.drawImage(video, 0, 0, video.width, video.height);
 
-        // getting the poses before drawing the image is important
-        const poses = await detector.estimatePoses(video, {flipHorizontal: false});
-        ctx.drawImage(video, 0, 0, video.width, video.height);
-
-        if (poses[0] && poses[0].keypoints.length > 0) {
-            drawKeypoints(poses[0].keypoints, ctx, CONFIDENCE)
-        }
-
-        requestAnimationFrame(drawVideo)
+    if (poses[0] && poses[0].keypoints.length > 0) {
+        drawKeypoints(poses[0].keypoints, ctx, CONFIDENCE)
     }
 
-    drawVideo()
+    console.log(pose)
 }
 
 const App = () => {
-    const [selectedPose, setSelectedPose] = useState(null);
+    const [pose, setPose] = useState(null);
+    const [config, setConfig] = useState(null)
+    const poseRef = useRef(pose);
+    const configRef = useRef(config)
 
     useEffect(() => {
-        start()
-    })
+        poseRef.current = pose;
+    }, [pose]);
+
+    const drawLoop = () => {
+        draw(poseRef.current, configRef.current);
+        requestAnimationFrame(drawLoop);
+    };
+
+    useEffect(() => {
+        getConfig().then(config => {
+            configRef.current = config
+            requestAnimationFrame(drawLoop)
+        })
+    }, []);
 
     const makeMenuCheckbox = (desc) => {
-        return {label: desc, onChange: () => setSelectedPose(desc), checked: selectedPose === desc}
+        return {label: desc, onChange: () => setPose(desc), checked: pose === desc}
     }
 
     const menuProps = [
@@ -45,8 +58,8 @@ const App = () => {
 
     return (
             <div>
-                <div>{selectedPose}</div>
-                <Video/>
+                <div>{pose}</div>
+                <Video />
                 <Menu props={menuProps}/>
             </div>
         )
