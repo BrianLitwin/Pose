fdbb328a8ae5e659cdd2feeda34c85c0d53e9c06
Brian Litwin <blitwin@horizoninvestments.com>
2023-10-02 19:00:23 -0400

    add utils to calculate poses

---
+++ src/body.js
@@ -0,0 +1,30 @@
+export function KpsToBodyparts(keypoints) {
+    return {
+      nose: keypoints[0],
+      leftEye: keypoints[1],
+      rightEye: keypoints[2],
+      leftEar: keypoints[3],
+      rightEar: keypoints[4],
+      leftShoulder: keypoints[5],
+      rightShoulder: keypoints[6],
+      leftElbow: keypoints[7],
+      rightElbow: keypoints[8],
+      leftWrist: keypoints[9],
+      rightWrist: keypoints[10],
+      leftHip: keypoints[11],
+      rightHip: keypoints[12],
+      leftKnee: keypoints[13],
+      rightKnee: keypoints[14],
+      leftAnkle: keypoints[15],
+      rightAnkle: keypoints[16],
+    }
+}
+
+function rightShinAngle(body) {
+    const start = body.rightAnkle
+
+    return angleBetweenLines(
+        {start, end: body.rightKnee},
+        {start, end: {x: start.x, y: start.y - 50}}
+    )
+}
